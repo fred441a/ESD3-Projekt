e16c4606323238fbf54e0db2e81193f85c255a00
Frederik Hansen <frederik@altofte.dk>
2023-11-02 11:13:13 +0100

    added memory map

---
+++ Read_Sensor/Read_Sensor.srcs/sources_1/new/Top.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/02/2023 10:18:55 AM
+-- Design Name: 
+-- Module Name: Top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Top is
+    Port ( 
+           clk : in STD_LOGIC;
+           scl : inout STD_LOGIC;
+           sda : inout STD_LOGIC;
+           
+           
+           );
+end Top;
+
+architecture Behavioral of Top is
+
+begin
+
+
+end Behavioral;
--- Readme.md
+++ Readme.md
@@ -1,2 +1,7 @@
 # Regler
  - Hvis nogen ødelægger master branch så skylder de sportscola i DE-klubben.
+
+# Memory map
+
+![memory map 1](./Assets/memoryOverview1.svg)
+![memory map 2](./Assets/memoryOverview2.svg)
